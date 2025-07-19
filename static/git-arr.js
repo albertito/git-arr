bedab61ba418482e886163cbcddf3f31f53678d8
Alberto Bertogli <albertito@blitiri.com.ar>
2025-07-19 11:31:37 +0100

    git-arr.js: Auto-format
    This patch uses js-beautify to auto-format git-arr.js, to make it easier
    to maintain a uniform style.

--- static/git-arr.js
+++ static/git-arr.js
@@ -10,28 +10,28 @@ function how_long_ago(timestamp) {
     if (timestamp < 0)
         return "never";
 
-	var seconds = Math.floor(now() - timestamp);
+    var seconds = Math.floor(now() - timestamp);
 
-	var interval = Math.floor(seconds / (365 * 24 * 60 * 60));
-	if (interval > 1)
-		return interval + " years ago";
+    var interval = Math.floor(seconds / (365 * 24 * 60 * 60));
+    if (interval > 1)
+        return interval + " years ago";
 
-	interval = Math.floor(seconds / (30 * 24 * 60 * 60));
-	if (interval > 1)
-		return interval + " months ago";
+    interval = Math.floor(seconds / (30 * 24 * 60 * 60));
+    if (interval > 1)
+        return interval + " months ago";
 
-	interval = Math.floor(seconds / (24 * 60 * 60));
+    interval = Math.floor(seconds / (24 * 60 * 60));
 
-	if (interval > 1)
-		return interval + " days ago";
-	interval = Math.floor(seconds / (60 * 60));
+    if (interval > 1)
+        return interval + " days ago";
+    interval = Math.floor(seconds / (60 * 60));
 
-	if (interval > 1)
-		return interval + " hours ago";
+    if (interval > 1)
+        return interval + " hours ago";
 
-	interval = Math.floor(seconds / 60);
-	if (interval > 1)
-		return interval + " minutes ago";
+    interval = Math.floor(seconds / 60);
+    if (interval > 1)
+        return interval + " minutes ago";
 
     if (seconds > 1)
         return Math.floor(seconds) + " seconds ago";
@@ -52,7 +52,7 @@ async function load_timestamps() {
     console.log("Loaded timestamps:", json);
 
     for (const [repo_name, timestamp] of Object.entries(json)) {
-        const e = document.getElementById("age:"+repo_name);
+        const e = document.getElementById("age:" + repo_name);
         if (!e) {
             console.warn(`No element found for repo: ${repo_name}`);
             continue;
